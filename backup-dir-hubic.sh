ca087197c5792e03327149b285adb7ac9b3b3363
pierre <pierre@NAS>
2017-02-23 14:15:29 +0100

    Backup: - addition in the tar.gz.gpg file of the hash of the file/folder name to backup, to avoid collision with the due to the deletion in these names of problematics characters (espacially the space character). Restore: - Addition of the managment of the name hash - Rewriting of the loop : a first loop is done and add informations in a array. Then a loop on the array restore files. Only one none-download swift command is executed now. - The rewriting of the loop allows to only take the last backup of a file/folder

--- backup-dir-hubic.sh
+++ backup-dir-hubic.sh
@@ -2,16 +2,18 @@
 
 passphrase=$1
 backupdir=$2
+# check foldername does not contains forbidden chars
 foldername=$3
-hubicpy=./hubic.py
+hubicpy=../hubic.py
 sha1sumFile="${backupdir}/.bkp_sha1sum.lst"
 starttime=$(date +"%Y%m%d_%H%M%S")
 
 echo `date +%Y%m%d\ %H:%M:%S` : Begining of script
 
 touch "${sha1sumFile}"
-ls "$BACKUP_DIR" | while read line; do
+ls "${backupdir}" | while read line; do
 
+  namehash=$(echo -n ${line} | md5sum | awk '{ print $1 }')
   filename=$(echo ${line} | cut -d'/' -f 1,2 | sed "s/[ \/,.'!?()]//g")
   safeLine=$(echo ${line} | sed -e 's/[][()\.^$?*+]/\\&/g')
   sha1=$(find "${backupdir}/${line}" -type f -print0 | sort -z | xargs -0 shasum | shasum | cut -d' ' -f 1)
@@ -31,19 +33,20 @@ ls "$BACKUP_DIR" | while read line; do
 
   # If its a file, the archive will be in the 'FILES' directory
   if [[ ! -d "${backupdir}/${line}" ]]; then
-    dest_path=sync_backups/${foldername}/FILES/${filename}_${starttime}.tar.gz.gpg
+    dest_path=sync_backups/${foldername}/FILES/${filename}_${namehash}_${starttime}.tar.gz.gpg
   else
-    dest_path=sync_backups/${foldername}/${filename}_${starttime}.tar.gz.gpg
+    dest_path=sync_backups/${foldername}/${filename}_${namehash}_${starttime}.tar.gz.gpg
   fi
 
   echo `date +%Y%m%d\ %H:%M:%S` : Compression will begin : ${filename}
   # Compression and encryption of the file/folder
-  tar -P -czf - "${backupdir}/${line}" | gpg --batch --yes --passphrase ${passphrase} -ac -o "/home/pierre/hubic/tmp/${filename}.tar.gz.gpg"
+  # TODO /home/pierre/hubic/tmp/ => ${4}/${backupdir}/ and delete/create it at the begining and the end of the script
+  tar -P -czf - "${backupdir}/${line}" | gpg --batch --yes --passphrase ${passphrase} -ac -o "/home/pierre/hubic/tmp/${filename}_${namehash}.tar.gz.gpg"
   echo `date +%Y%m%d\ %H:%M:%S` : Upload will begin      : ${filename}
 
   #Calculate the size of swift segment to optimize the number of segment (empirical, around 100. 1000 is the maximum allowed)
   #TODO parameterize this
-  actualsize=$(du -k "/home/pierre/hubic/tmp/${filename}.tar.gz.gpg" | cut -f 1)
+  actualsize=$(du -k "/home/pierre/hubic/tmp/${filename}_${namehash}.tar.gz.gpg" | cut -f 1)
   if [ ${actualsize} -le 5000000 ]; then
     segmentSize=30000000
   elif [ ${actualsize} -le 10000000 ]; then
@@ -55,19 +58,20 @@ ls "$BACKUP_DIR" | while read line; do
   fi
 
   #Upload of the encrypted file
-  $hubicpy --swift -- upload --use-slo --segment-size ${segmentSize} --object-threads 40 --segment-threads 20 --object-name "${dest_path}" default /home/pierre/hubic/tmp/${filename}.tar.gz.gpg
+echo   $hubicpy --swift -- upload --use-slo --segment-size ${segmentSize} --object-threads 40 --segment-threads 20 --object-name "${dest_path}" default /home/pierre/hubic/tmp/${filename}_${namehash}.tar.gz.gpg
+  $hubicpy --swift -- upload --use-slo --segment-size ${segmentSize} --object-threads 40 --segment-threads 20 --object-name "${dest_path}" default /home/pierre/hubic/tmp/${filename}_${namehash}.tar.gz.gpg
   echo `date +%Y%m%d\ %H:%M:%S` : Upload done            : ${filename}
   #TODO check the size of the uploaded file and compare with the local file size  
   if [ $? -eq 0 ]; then
     echo "${sha1} ${line}"
     echo "${sha1} ${line}" >> "${sha1sumFile}"
   else
-    $hubicpy --swift -- list default_segments | grep "sync_backups/$(basename ${backupdir})/${filename}.tar.gz.gpg" | xargs -I {} sh -c "${hubicpy} --swift delete {}"
+    $hubicpy --swift -- list default_segments | grep "sync_backups/$(basename ${backupdir})/${filename}_${namehash}.tar.gz.gpg" | xargs -I {} sh -c "${hubicpy} --swift delete {}"
     exit 1
     #TODO Do not exit the script but continu and warn of the error at the end of the script
   fi
 
-rm -f /home/pierre/hubic/tmp/${filename}.tar.gz.gpg
+rm -f /home/pierre/hubic/tmp/${filename}_${namehash}.tar.gz.gpg
 echo `date +%Y%m%d\ %H:%M:%S` : File processed         : ${filename}
 done
 
--- restore-dir-hubic.sh
+++ restore-dir-hubic.sh
@@ -5,7 +5,7 @@ echo `date +%Y%m%d\ %H:%M:%S` : Begining of script
 passphrase=$1
 backupdir=$2
 foldername=$3
-hubicpy=./hubic.py
+hubicpy=../hubic.py
 sha1sumFile="${backupdir}/.bkp_sha1sum.lst"
 
 #TODO tests of $2 local folder and $3 distant folder existance
@@ -14,8 +14,64 @@ sha1sumFile="${backupdir}/.bkp_sha1sum.lst"
 mkdir -p /media/wd0/tests/${foldername}
 cd /media/wd0/tests/${foldername}
 
+# FILES is an array with the name file without the date as index
+# The associated values are the dates concatenated with the size
+declare -A FILES
+
+# hubic.py returns a value for each file AND column of the list swift command
+# so we need a column counter. Column 0 is size and column 3 is name. 
+# Column 1 and 2 are date and hour, we do not need it
+column=0
 # For all files in the designated distant folder
-for gpg_file_path in `~/hubic/hubic.py --swift -- list default -p sync_backups/${foldername}/`; do
+for line in `~/hubic/hubic.py --swift -- list default -p sync_backups/${foldername}/ -l`;
+do
+	if [ ${column} -eq 0 ]
+	then
+		size_remote=${line}
+	elif [ ${column} -eq 3 ]
+	then
+		filename=${line}
+		# Get the file name without the date, to be the index of the array
+		echo filename : ${filename}
+		filenamewodate=${filename::-27}
+		# The date, to compare with another instance of the file
+		date_tmp=${filename: -26}
+		date=${date_tmp::-11}
+
+		# Is there this file in the array already ?
+		if [ ${FILES[${filenamewodate}]+_} ];
+		then
+			value=${FILES[${filenamewodate}]}
+			# Date of the file in the array
+			prev_date=${value:0:15}
+			# Is the current file more recent ?
+			if [ ${date//_} -gt ${prev_date//_} ]
+			then
+				# The current file is more recent, we keep it
+				echo This file has a newer version existing and will not be downloaded : ${filenamewodate}_${prev_date}.tar.gz.gpg
+				FILES[${filenamewodate}]=${date}_${size_remote}
+			else
+				echo This file has a newer version existing and will not be downloaded : ${filename}
+			fi
+		else
+			# NO other file in the array, we keep it
+			#echo File ${filenamewodate} added to the list
+			FILES[${filenamewodate}]=${date}_${size_remote}
+		fi
+	fi
+	column=$(((column+1)%4));
+done
+
+#number of values
+#echo number of values : ${#FILES[@]}
+#calculer le nombre total de dl à faire
+
+for filenamewodate in "${!FILES[@]}";
+do
+	value=${FILES[${filenamewodate}]}
+	gpg_file_path=${filenamewodate}_${value:0:15}.tar.gz.gpg
+	size_remote=${value:16}
+
 	# Sometimes the for loop goes crazy, i do not know why, yet. So, little check
 	if [[ ${gpg_file_path} != *"${foldername}"* ]]
 	then
@@ -23,17 +79,15 @@ for gpg_file_path in `~/hubic/hubic.py --swift -- list default -p sync_backups/$
 		continue
 	fi
 	
-	# Get the size of the distant file, to compare to a previous execution downloaded file and/or the futur downloaded file
-	length_remote=$(~/hubic/hubic.py --swift -- stat default ${gpg_file_path} | grep "Content Length" | cut -d' ' -f11)
-	echo `date +%Y%m%d\ %H:%M:%S` : The file has a size of ${length_remote=} : ${gpg_file_path}
+	echo `date +%Y%m%d\ %H:%M:%S` : The file has a size of ${size_remote=} : ${gpg_file_path}
 	# If file already exists AND local file size equals distant file size
-	if test -f "${gpg_file_path}" && test "${length_remote}" -eq "$(du -b "${gpg_file_path}" | cut -f 1)"
+	if test -f "${gpg_file_path}" && test "${size_remote}" -eq "$(du -b "${gpg_file_path}" | cut -f 1)"
 	then
 		echo `date +%Y%m%d\ %H:%M:%S` : The file has already been succesfully downloaded : ${gpg_file_path}
 		continue
 	fi
 	# If file already exists AND local file size NOT equals distant file size
-	if test -f "${gpg_file_path}" && test "${length_remote}" -ne "$(du -b "${gpg_file_path}" | cut -f 1)"	
+	if test -f "${gpg_file_path}" && test "${size_remote}" -ne "$(du -b "${gpg_file_path}" | cut -f 1)"	
        	then
 		echo `date +%Y%m%d\ %H:%M:%S` : Sizes do not match, the file is removed  : ${gpg_file_path}
 		rm -f ${gpg_file_path}
@@ -46,7 +100,7 @@ for gpg_file_path in `~/hubic/hubic.py --swift -- list default -p sync_backups/$
 	# Size of the downloaded file, for comparison
 	actualsize=$(du -b "${gpg_file_path}" | cut -f 1)
 	# If the size of the downloaded file does not matche the size of the distant file		
-	if [ "${length_remote}" -ne "${actualsize}" ]
+	if [ "${size_remote}" -ne "${actualsize}" ]
 	then
 		echo `date +%Y%m%d\ %H:%M:%S` : ERROR Download KO. Size=${actualsize}
 		continue
@@ -67,12 +121,13 @@ for gpg_file_path in `~/hubic/hubic.py --swift -- list default -p sync_backups/$
 	echo `date +%Y%m%d\ %H:%M:%S` : The file will be decompressed : ${gpg_file_path::-4}
 		
 	# Decompression of the file
-	tar -xzvf ${gpg_file_path::-4}
+	tar -xzvf --no-overwrite-dir ${gpg_file_path::-4}
 
 	# Deletion of the file
 	rm -f ${gpg_file_path::-4} 
 done 
 
+echo `date +%Y%m%d\ %H:%M:%S` : Comparison between original folder and downloaded backup
 echo ------------------------------------------------------------
 echo ------------------------------------------------------------
 echo ---------------------- COMPARISON --------------------------
